a78c8b6f5cec22e84e1d3cd9b9c0fa4809d5ba23
PDKhan <kkzoro1@gmail.com>
2025-04-07 19:55:21 -0700

    98. Validate Binary Search Tree solution

---
+++ validate-binary-search-tree.c
@@ -0,0 +1,13 @@
+bool search(struct TreeNode* root, long min, long max){
+    if(root == NULL)
+        return true;
+    
+    if(root->val <= min || root->val >= max)
+        return false;
+    
+    return search(root->left, min, root->val) && search(root->right, root->val, max);
+}
+
+bool isValidBST(struct TreeNode* root) {
+    return search(root, (long)INT_MIN-1, (long)INT_MAX+1);
+}
