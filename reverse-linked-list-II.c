52fe7e0129575d63e0fd271a0432737d675f26a2
PDKhan <kkzoro1@gmail.com>
2025-07-21 13:03:47 -0500

    92. Reverse Linked List II

--- reverse-linked-list-II.c
+++ reverse-linked-list-II.c
@@ -1,42 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
 struct ListNode* reverseBetween(struct ListNode* head, int left, int right) {
-    int i = 1;
-    int top = 0;
-    struct ListNode* curr = head;
-    struct ListNode* prev = NULL;
-    struct ListNode* stack[500];
+    int cnt = 1;
+    struct ListNode* r_head = NULL;
+    struct ListNode* r_tail;
+    struct ListNode* new_head = NULL;
+    struct ListNode* tail;
+    struct ListNode* search = head;
 
-    while(curr){
-        struct ListNode* next = curr->next;
+    while(search){
+        struct ListNode* next = search->next;
 
-        curr->next = NULL;
+        search->next = NULL;
 
-        if(left <= i && i < right){
-            stack[top++] = curr;
-        }else if(i == right){
-            if(prev == NULL){
-                prev = curr;
-                head = prev;
-            }else{
-                prev->next = curr;
-                prev = prev->next;
-            }
+        if(cnt >= left && cnt <= right){
+            search->next = r_head;
+            r_head = search;
 
-            while(top){
-                    prev->next = stack[--top];
-                    prev = prev->next;
+            if(cnt == left)
+                r_tail = r_head;
+
+            if(cnt == right){
+                if(new_head == NULL)
+                    new_head = r_head;
+                else
+                    tail->next = r_head;
+                tail = r_tail;
             }
         }else{
-            if(prev == NULL){
-                prev = curr;
+            if(new_head == NULL){
+                new_head = search;
+                tail = new_head;
             }else{
-                prev->next = curr;
-                prev = prev->next;
+                tail->next = search;
+                tail = tail->next;
             }
         }
 
-        i++;
-        curr = next;
+        cnt++;
+        search = next;
     }
 
-    return head;
+    if(new_head)
+        return new_head;
+    else
+        return r_head;
 }
