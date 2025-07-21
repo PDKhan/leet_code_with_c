95fb6c1c16ec458f2edba8f5e1181b4c4ffa132f
PDKhan <kkzoro1@gmail.com>
2025-07-21 15:30:03 -0500

    104. Maximum Depth of Binary Tree

--- maximum-depth-of-binary-tree.c
+++ maximum-depth-of-binary-tree.c
@@ -1,28 +1,9 @@
 int maxDepth(struct TreeNode* root) {
-    int depth = 0;
-    int first = 0, last = 0;
-    struct TreeNode* q[100000];
-
     if(root == NULL)
-        return depth;
+        return 0;
+    
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
 
-    q[last++] = root;
-
-    while(first != last){
-        int len = last - first;
-
-        for(int i = 0; i < len; i++){
-            struct TreeNode* curr = q[first++];
-
-            if(curr->left)
-                q[last++] = curr->left;
-            
-            if(curr->right)
-                q[last++] = curr->right;
-        }
-
-        depth++;
-    }
-
-    return depth;
+    return 1 + (left > right ? left : right);
 }
